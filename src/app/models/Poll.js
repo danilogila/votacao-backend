4cd6aca31b83a76aefd636b562efb96a2f6e35ac
Danilo Gila de Santana <danilosantana58@gmail.com>
2019-11-01 01:49:52 -0300

    Create new Schema for options

--- src/app/models/Poll.js
+++ src/app/models/Poll.js
@@ -1,5 +1,20 @@
 const mongoose =  require('mongoose')
 const { Schema } = mongoose
+
+const ChoiceSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    votes: {
+        type: Number,
+        default: 0
+    },
+    avatar: {
+        type: String
+    }
+})
+
 const PollSchema = new Schema({
     author: {
         type: String,
@@ -23,46 +38,7 @@ const PollSchema = new Schema({
         type: Date,
         default: Date.now
     },
-    options: [
-        {   
-            optionId: {
-                type: mongoose.Schema.ObjectId, 
-                auto: true 
-            },
-            value: {
-                type: String,
-                required: true
-            },
-            votes: {
-                type: Number,
-                default: 0
-            }
-        }
-    ]
+    choices: [ChoiceSchema]
 })
-
-// PollSchema.methods = {
-//     vote(pollId, optionId){
-//         // return bcrypt.compare(password, this.password)
-//         console.log(`PollId: ${pollId} OptionId: ${optionId}`)
-//         console.log(this)
-//     }
-// }
-
-PollSchema.methods.submitVote = function(pollId, optionId) {
-    var increment = {
-        $inc: {
-            'choices.$.votes': 1
-        }
-    };
-    var query = {
-        '_id': pollId,
-        'choices._id': optionId,
-    };
-    console.log("ahhhhh")
-    let teste = this.model('Poll').update(query, increment);
-    console.log(teste)
-    return this.model('Poll').update(query, increment);
-};
 
 module.exports = mongoose.model('Poll', PollSchema)
