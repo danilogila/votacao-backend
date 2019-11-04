1ec17ad9e49e7cac7892eef5d6a964879df98330
Danilo Gila de Santana <danilosantana58@gmail.com>
2019-11-04 17:55:44 -0300

    Add new routes

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const routes = express.Router()
 
-const authMiddleware = require('./app/middlewares/auth')
+// const authMiddleware = require('./app/middlewares/auth')
 
 const controllers = require('./app/controllers/')
 
@@ -10,13 +10,16 @@ routes.get('/polls', controllers.PollController.index)
 routes.get('/polls/:pollId', controllers.PollController.show)
 routes.post('/polls/new', controllers.PollController.store)
 routes.put('/polls/:pollId', controllers.PollController.update)
-routes.delete('/polls/:id', controllers.PollController.delete)
+routes.delete('/polls/:pollId', controllers.PollController.delete)
 routes.post('/polls/:pollId/vote/:optionId', controllers.PollController.vote)
+// routes.post('/polls/filter', controllers.PollController.filter)
 
+routes.get('/users', controllers.UserController.index)
 routes.post('/users', controllers.UserController.store)
 routes.post('/sessions', controllers.SessionController.store)
 
-routes.use(authMiddleware)
-routes.get('/teste', authMiddleware, (req, res) => res.json({ ok: true }))
+// routes.use(authMiddleware)
+// routes.get('/teste', authMiddleware, (req, res) => res.json({ ok: true }))
 
+routes.get('/*', (req, res) => res.send('Hello World'))
 module.exports = routes
