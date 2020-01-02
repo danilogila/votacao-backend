663c0b32d802de6ff6b8733a9657bd86713ff911
Danilo Gila de Santana <danilosantana58@gmail.com>
2020-01-01 21:10:56 -0300

    Implements cluster logic for performance improvement

--- src/index.js
+++ src/index.js
@@ -1,9 +1,30 @@
 const server = require('./server')
 const chalk = require('chalk')
+const cluster = require('cluster')
+let numWorkers = require('os').cpus().length;
 
 let PORT = process.env.PORT || 3000
 
-server.listen(PORT, () => {
-    console.log('%s APP is running at http://localhost:%d', chalk.green('✓'), PORT);
-    console.log('  Press CTRL-C to stop\n');
+if(cluster.isMaster) {
+  console.log('Master cluster setting up ' + numWorkers + ' workers...');
+
+  for(var i = 0; i < numWorkers; i++) {
+      cluster.fork();
+  }
+
+  cluster.on('online', function(worker) {
+      console.log('Worker ' + worker.process.pid + ' is online');
   });
+
+  cluster.on('exit', function(worker, code, signal) {
+      console.log('Worker ' + worker.process.pid + ' died with code: ' + code + ', and signal: ' + signal);
+      console.log('Starting a new worker');
+      cluster.fork();
+  });
+} else {
+  server.listen(PORT, () => {
+      console.log('%s APP is running at http://localhost:%d', chalk.green('✓'), PORT);
+      console.log(`APP is running at cluster: ${cluster.worker.id}`)
+      console.log('  Press CTRL-C to stop\n');
+    });
+}
