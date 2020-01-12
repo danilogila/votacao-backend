5cee1224bbaf61eb5e39ca0965113af5ee743f78
Danilo Gila de Santana <danilosantana58@gmail.com>
2020-01-11 22:13:20 -0300

    Orgnize routes

--- src/routes.js
+++ src/routes.js
@@ -2,16 +2,19 @@ const express = require('express')
 const routes = express.Router()
 const controllers = require('./app/controllers/')
 
-// URL pra Votaçao Atual, podendo votar em uma opção
+// Listar Votações
+routes.get('/polls', controllers.PollController.index)
+
+// Votar em uma opção
 routes.post('/polls/:pollId/vote/:optionId', controllers.VoteController.vote)
 
-// Dados de uma votacao
+// Mostrar votacao
 routes.get('/polls/:pollId', controllers.PollController.show)
 
-// Quantidade de votos de uma votação
+// Mostrar total de votos de uma votação
 routes.get('/polls/:pollId/total', controllers.VoteController.total)
 
-// Total de votos por candidator
+// Mostrar total de votos de um candidato 
 routes.get('/polls/:pollId/candidate/:optionId/total', controllers.VoteController.totalCandidate)
 
 // Criar uma votação
@@ -25,9 +28,6 @@ routes.delete('/polls/:pollId', controllers.PollController.delete)
 
 // Buscar tempo restante da votação
 routes.get('/polls/:pollId/time', controllers.PollController.findRemainTime)
-
-// Listar Votações
-routes.get('/polls', controllers.PollController.index)
 
 // Listar quantidade de votações por hora
 routes.get('/polls/:pollId/votes/hour', controllers.VoteController.totalHour)
